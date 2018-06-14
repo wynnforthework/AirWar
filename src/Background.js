6ac6b68033e5a908084667c291c4ee4ba174dcf8
wynn <wynn@MacBook-Pro.local>
2018-06-15 07:11:22 +0800

    添加第二张图片，循环滚动

--- src/Background.js
+++ src/Background.js
@@ -6,7 +6,26 @@ var Background = (function(_super){
         this.addChild(this.bg1);
 
         console.log("加载背景")
+
+        this.bg2 = new Laya.Sprite();
+        this.bg2.loadImage("war/background.png");
+        this.bg2.pos(0,-852);
+        this.addChild(this.bg2);
+
+        Laya.timer.frameLoop(1,this,this.onLoop);
     }
     Laya.class(Background, "Background",_super);
+
+    var _proto = Background.prototype;
+    _proto.onLoop = function(){
+        this.y += 10;
+        if(this.bg1.y+this.y>=852){
+            this.bg1.y -= 852*2;
+        }
+        if(this.bg2.y+this.y>=852){
+            this.bg2.y -= 852*2;
+        }
+    }
+
     return Background;
 })(Laya.Sprite);
