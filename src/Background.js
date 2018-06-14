e477372aa0cef8c704f275183548595e7b1f9f8f
wynn <wynn@MacBook-Pro.local>
2018-06-15 06:42:49 +0800

    忽略chrome调试文件

--- src/Background.js
+++ src/Background.js
@@ -1,5 +1,6 @@
 var Background = (function(_super){
     function Background(){
+        Background.super(this);
         this.bg1 = new Laya.Sprite();
         this.bg1.loadImage("war/background.png");
         this.addChild(this.bg1);
--- src/LayaSample.js
+++ src/LayaSample.js
@@ -1,9 +1,16 @@
-var WebGL = laya.webgl.WebGL;
-Laya.init(600, 400, WebGL);
-
 (function(){
     (()=>{
         var WebGL = laya.webgl.WebGL;
-        Laya.init(600,400,WebGL);
+        Laya.init(400,852,WebGL);
+        this.bg = new Background();
+        Laya.stage.addChild(this.bg);
+
+        Laya.stage.scaleMode = Laya.Stage.SCALE_NOSCALE;
+        Laya.stage.alignH = Laya.Stage.ALIGN_CENTER;
+        Laya.stage.alignV = Laya.Stage.ALIGN_MIDDLE;
+        Laya.stage.screenMode = Laya.Stage.SCREEN_NONE;
+
+        Laya.Stat.show(0,0);
+
     });
 })();
